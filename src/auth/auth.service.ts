89c648745882c2898c18598dec6014a647dce628
riefive <rie.five@gmail.com>
2023-11-10 17:10:45 +0700

    chore: initialized a auth sample

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private jwtService: JwtService,
+        private usersService: UsersService
+    ) {}
 
     async signIn(username: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(username);
         if (user?.password !== pass) {
             throw new UnauthorizedException();
         }
-        const { password, ...result } = user;
-        // TODO: Generate a JWT and return it here
-        // instead of the user object
-        return result;
+        const payload = { sub: user.userId, username: user.username };
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        };
     }
 }
