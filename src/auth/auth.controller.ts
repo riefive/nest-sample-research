654929078b87d4543a5068b6bdf840871dac5ed9
riefive <rie.five@gmail.com>
2023-11-13 09:17:07 +0700

    chore: implement public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,22 +6,24 @@ import {
     Get,
     Post,
     Request,
-    UseGuards 
+    // UseGuards 
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
+// import { AuthGuard } from './auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    @Public()
     @HttpCode(HttpStatus.OK)
     @Post('login')
     signIn(@Body() signInDto: Record<string, any>) {
         return this.authService.signIn(signInDto.username, signInDto.password);
     }
 
-    @UseGuards(AuthGuard)
+    // @UseGuards(AuthGuard)
     @Get('profile')
     getProfile(@Request() req) {
         return req.user;
